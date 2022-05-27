1c42ea9b4ac3a80f19984ac2d3c7f901e54b6511
Andy <tambe.ani@northeastern.edu>
2022-05-27 16:21:53 -0400

    added cloudformation & generator script

---
+++ src/main/java/com/iot/iotservice/IotserviceApplication.java
@@ -0,0 +1,13 @@
+package com.iot.iotservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IotserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IotserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iot/iotservice/IotserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iot.iotservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IotserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
